f2976fbadc993c343c43bef0ddfe42a3d499e6d7
Bhavya Dang <bhavya.dang127@gmail.com>
2026-06-29 15:02:39 +0530

    Add pkgui formula v0.0.1

---
+++ Formula/pkgui.rb
@@ -0,0 +1,13 @@
+class Pkgui < Formula
+  desc "TUI to manage multiple package managers and packages installed by them"
+  homepage "https://github.com/bhavya-dang/pkgui"
+  url "https://github.com/bhavya-dang/pkgui/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "de3be059fea62a1063e10f616f4c9eb0dfc544bbffe9470be047d3b2ca4cbe20"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+end
