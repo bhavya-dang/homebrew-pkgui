5cb4c410d5f1e954b877bb86dd6e1244a4b0a9ef
Bhavya Dang <bhavya.dang127@gmail.com>
2026-07-07 17:08:38 +0530

    updated version to v0.0.2

--- Formula/pkgui.rb
+++ Formula/pkgui.rb
@@ -1,7 +1,7 @@
 class Pkgui < Formula
   desc "TUI to manage multiple package managers and packages installed by them"
   homepage "https://github.com/bhavya-dang/pkgui"
-  url "https://github.com/bhavya-dang/pkgui/archive/refs/tags/v0.0.1.tar.gz"
+  url "https://github.com/bhavya-dang/pkgui/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "de3be059fea62a1063e10f616f4c9eb0dfc544bbffe9470be047d3b2ca4cbe20"
   license "MIT"
 
