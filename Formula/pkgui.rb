f83faf1c4796981be8993fa3f73df8bc8c2e740c
Bhavya Dang <bhavya.dang127@gmail.com>
2026-07-07 17:19:27 +0530

    updated hash

--- Formula/pkgui.rb
+++ Formula/pkgui.rb
@@ -2,7 +2,7 @@ class Pkgui < Formula
   desc "TUI to manage multiple package managers and packages installed by them"
   homepage "https://github.com/bhavya-dang/pkgui"
   url "https://github.com/bhavya-dang/pkgui/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "de3be059fea62a1063e10f616f4c9eb0dfc544bbffe9470be047d3b2ca4cbe20"
+  sha256 "08ac33c6a59766e73f44e23592b84b18b18d0a06e0af44463773cca15b6b43f3"
   license "MIT"
 
   depends_on "go" => :build
